82ee3f31b31020ca0fb6091360d153c090a38dcc
Timo Bolte <timo.bolte@hhu.de>
2021-12-07 00:46:44 +0100

    Day 06 Changes

--- AdventOfCode2021/src/Day06/Day06_2ndPart.java
+++ AdventOfCode2021/src/Day06/Day06_2ndPart.java
@@ -9,7 +9,15 @@ public class Day06_2ndPart {
     static int days=256;
 
     public static void main(String[] args) {
-        List<Lanternfish> fish = Arrays.stream(args[0].split(","))
+        init(args[0]);
+        for(int i = 0; i< days; i++) {
+            populationCycle();
+        }
+        calcPopulation();
+    }
+
+    private static void init(String arg) {
+        List<Lanternfish> fish = Arrays.stream(arg.split(","))
                 .mapToInt(Integer::parseInt)
                 .mapToObj(x -> new Lanternfish(x))
                 .collect(Collectors.toList());
@@ -21,21 +29,22 @@ public class Day06_2ndPart {
                 }
             }
         }
-        Arrays.stream(daysLeft).forEach(System.out::println);
-        for(int i = 0; i< days; i++) {
-            long zwischenSpeicher = daysLeft[0];
-            daysLeft[0]=daysLeft[1];
-            daysLeft[1]=daysLeft[2];
-            daysLeft[2]=daysLeft[3];
-            daysLeft[3]=daysLeft[4];
-            daysLeft[4]=daysLeft[5];
-            daysLeft[5]=daysLeft[6];
-            daysLeft[6]=daysLeft[7]+zwischenSpeicher;
-            daysLeft[7]=daysLeft[8];
-            daysLeft[8]=zwischenSpeicher;
-            System.out.println("Next --------");
-            Arrays.stream(daysLeft).forEach(System.out::println);
-        }
+    }
+
+    private static void populationCycle() {
+        long zwischenSpeicher = daysLeft[0];
+        daysLeft[0]=daysLeft[1];
+        daysLeft[1]=daysLeft[2];
+        daysLeft[2]=daysLeft[3];
+        daysLeft[3]=daysLeft[4];
+        daysLeft[4]=daysLeft[5];
+        daysLeft[5]=daysLeft[6];
+        daysLeft[6]=daysLeft[7]+zwischenSpeicher;
+        daysLeft[7]=daysLeft[8];
+        daysLeft[8]=zwischenSpeicher;
+    }
+
+    private static void calcPopulation() {
         long summe=daysLeft[0];
         for(int i=1;i<9;i++){
             summe+=daysLeft[i];
